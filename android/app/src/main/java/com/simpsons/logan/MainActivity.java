91e335965d6fca12251f76768ea2f68a921fcfa2
Logan-Gaillard <logangaillard1@gmail.com>
2026-01-20 19:17:52 +0100

    Ajout d'android

---
+++ android/app/src/main/java/com/simpsons/logan/MainActivity.java
@@ -0,0 +1,5 @@
+package com.simpsons.logan;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from "@capacitor/cli";
+
+const config: CapacitorConfig = {
+	appId: "com.simpsons.logan",
+	appName: "thesimpsonapi",
+	webDir: "dist",
+};
+
+export default config;
